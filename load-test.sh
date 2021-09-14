04d545c2c999c17b4ea8ef9a9fd4b9fd53739e92
David Scrobonia <davidscrobonia@gmail.com>
2021-09-13 22:05:00 -0700

    last thing

--- load-test.sh
+++ load-test.sh
@@ -6,6 +6,7 @@ echo $1 $HEAD
   --api-token=2b2e2gfq4w1x6u4n6q1h5l6p5r3d464c6246r1o3l27b4wv6p5 \
   --report-upload \
   --branch testing-something-out \
+  --main-branch testing-something-out \
   --project github/david-test-org-2/railsgoat \
   --command "sh ./script.sh $HEAD" \
   --require-full-repo \
